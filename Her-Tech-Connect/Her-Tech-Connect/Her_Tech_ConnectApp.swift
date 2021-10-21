177cfeb672d2c92d05c01f1e26629be22beb2d5c
NatalmanNah <87883622+NatalmanNah@users.noreply.github.com>
2021-10-20 20:58:46 -0500

    Create a workspace for the project, add pod file to it, and connect it to firebase

---
+++ Her-Tech-Connect/Her-Tech-Connect/Her_Tech_ConnectApp.swift
@@ -0,0 +1,28 @@
+//
+//  Her_Tech_ConnectApp.swift
+//  Her-Tech-Connect
+//
+//  Created by Natalman Nahm on 10/20/21.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct Her_Tech_ConnectApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    print("Colors application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+    FirebaseApp.configure()
+    return true
+  }
+}
