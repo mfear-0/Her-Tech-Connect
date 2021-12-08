27e66c17d74d29ed6b0c6e287c7c14765b1fdfeb
mfear-0 <fearkenz@gmail.com>
2021-12-08 13:38:36 -0800

    Added event handler and database integration. Added events now get stored in the firebase rtdb.

---
+++ Her-Tech-Connect/Her-Tech-Connect/EventHandler.swift
@@ -0,0 +1,33 @@
+//
+//  EventHandler.swift
+//  Her-Tech-Connect
+//
+//  Created by Student Account on 12/8/21.
+//
+
+import Foundation
+import FirebaseDatabase
+
+class EventHandler {
+    static let eventID = UUID().uuidString
+    static let ref = Database.database().reference()
+    
+    static func addEvent(name: String, address: String, date: String, time:String) {
+        print("Reached event handler")
+        
+        let event: [String: Any] = [
+            "eventID": eventID,
+            "name": name,
+            "date": date,
+            "time": time,
+            "address": address
+            
+        ]
+        
+        ref.child("Events").child(eventID).setValue(event)
+    }
+    
+    
+    
+    
+}
