4f0ec46cf06015e3aeb805d27433d38b252b0607
NatalmanNah <87883622+NatalmanNah@users.noreply.github.com>
2022-04-22 20:34:18 -0500

    Get the Homepage tp display the 2 latest shoutouts and added pull to refresh to get auto updates.

--- Her-Tech-Connect/Her-Tech-Connect/HomePage.swift
+++ Her-Tech-Connect/Her-Tech-Connect/HomePage.swift
@@ -4,44 +4,105 @@
 //
 //  Created by Natalman Nahm on 10/23/21.
 //  Modified by Arica Conrad on 12/15/21.
-//
+//  Modified by Natalman Nahm on 04/22/22
 
 import SwiftUI
 import FirebaseAuth
+import FirebaseDatabase
 
 struct HomePage: View {
+    @ObservedObject var newShoutouts: AllShoutOuts = AllShoutOuts()
+    let ref = Database.database().reference()
+    @State private var showingSheet = false
     @EnvironmentObject var viewModel: AuthViewModel
     var body: some View {
         
-        VStack {
-            
-            // Arica: A decorative ombre band across the top that showcases the app's colors.
-            ZStack {
-                
-                Rectangle()
-                    .fill(LinearGradient(gradient: Gradient(colors: [Color("LightBlue"), Color("LightYellow")]), startPoint: .leading, endPoint: .trailing))
-                    .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 20)
-            }
-            
-            Spacer()
-            
-            ScrollView {
-                
-                VStack {
+//        VStack {
+//
+//            // Arica: A decorative ombre band across the top that showcases the app's colors.
+//            ZStack {
+//
+//                Rectangle()
+//                    .fill(LinearGradient(gradient: Gradient(colors: [Color("LightBlue"), Color("LightYellow")]), startPoint: .leading, endPoint: .trailing))
+//                    .frame(minWidth: 0, maxWidth: .infinity, maxHeight: 20)
+//            }
+//
+//            Spacer()
+//
+//            ScrollView {
+//
+//                VStack {
+//
+//                    // Arica: The title text.
+//                    Text("Welcome to Her Tech Connect!")
+//                        .foregroundColor(Color("DarkBlue"))
+//                        .font(.title2)
+//                        .padding()
+//
+//                    // Arica: Instructional text for the users.
+//                    Text("Here are the highlights of what happened while you were away:")
+//                        .foregroundColor(Color("Black"))
+//                        .font(.body)
+//                        .padding()
+//
+//                    // Arica: The temporary location of the log out button. Please move to the bottom of the More tab.
+//                    Button(action: {
+//                        viewModel.signOut()
+//                    }, label: {
+//                        Text("Log Out")
+//                            .padding()
+//                            .foregroundColor(Color("Black"))
+//                            .font(.title3)
+//                            .frame(minWidth: 0, maxWidth: .infinity)
+//                            .background(Color("LightBlueSwitch"))
+//                            .cornerRadius(40)
+//                            .overlay( RoundedRectangle(cornerRadius: 40)
+//                            .stroke(Color("LightBlue"), lineWidth: 4))
+//                    })
+//                    .padding()
+//
+//                    Spacer()
+//                }
+//            }
+//        }
+        
+        ZStack{
+            VStack{
+                ScrollView(.vertical, showsIndicators: false){
                     
-                    // Arica: The title text.
-                    Text("Welcome to Her Tech Connect!")
-                        .foregroundColor(Color("DarkBlue"))
-                        .font(.title2)
-                        .padding()
+                    RefreshControl(coordinateSpace: .named("RefreshControl")) {
+                        //refresh view
+                        newShoutouts.Shoutdata.removeAll()
+                        DispatchQueue.main.async {
+                            getEarlyShoutOut()
+                        }
+                        
+                    }
                     
-                    // Arica: Instructional text for the users.
-                    Text("Here are the highlights of what happened while you were away:")
-                        .foregroundColor(Color("Black"))
-                        .font(.body)
-                        .padding()
+                    HStack{
+                        Rectangle()
+                            .fill(Color.blue)
+                            .frame(maxWidth: .infinity, maxHeight: 2.0)
+                        Text("Latest Shoutouts")
+                            .font(.system(size: 15))
+                            .foregroundColor(Color.blue)
+                        Rectangle()
+                            .fill(Color.blue)
+                            .frame(maxWidth: .infinity, maxHeight: 2.0)
+                    }
+                    .padding()
                     
-                    // Arica: The temporary location of the log out button. Please move to the bottom of the More tab.
+                    LazyVStack(alignment: .leading, spacing: 0){
+                        ForEach(newShoutouts.Shoutdata.indices.prefix(2), id: \.self){ index in
+                            ShoutOutCard(shoutOut: newShoutouts.Shoutdata[index])
+                        }
+                    }
+                    .onAppear(perform: {
+                        if newShoutouts.Shoutdata.isEmpty{
+                            getEarlyShoutOut()
+                        }
+                    })
+                    
                     Button(action: {
                         viewModel.signOut()
                     }, label: {
@@ -56,12 +117,31 @@ struct HomePage: View {
                             .stroke(Color("LightBlue"), lineWidth: 4))
                     })
                     .padding()
-                    
-                    Spacer()
                 }
+                .coordinateSpace(name: "RefreshControl")
             }
         }
-        .background(Color("White").ignoresSafeArea())
+        
+    }
+    
+    //load the view with existing shoutouts
+    func getEarlyShoutOut() {
+        let sorted = ref.child("ShoutOut").queryOrdered(byChild: "timeCreated")
+        sorted.observeSingleEvent(of: .value, with: {(post) in
+            for aPost in post.children {
+                let snap = aPost as! DataSnapshot
+                let postDict = snap.value as! [String: Any]
+                
+                if let image = postDict["image"] as? String {
+                    newShoutouts.Shoutdata.append(ShoutOut(shoutOutID: postDict["shoutOutID"] as! String, posterID: postDict["posterID"] as! String, title: postDict["title"] as! String, story: postDict["story"] as! String, timeCreated: postDict["timeCreated"] as! Double, upvotes: postDict["upVotes"], image: image))
+                } else {
+                    newShoutouts.Shoutdata.append(ShoutOut(shoutOutID: postDict["shoutOutID"] as! String, posterID: postDict["posterID"] as! String, title: postDict["title"] as! String, story: postDict["story"] as! String, timeCreated: postDict["timeCreated"] as! Double, upvotes: postDict["upVotes"], image: ""))
+
+                }
+            }
+            newShoutouts.Shoutdata.reverse()
+            
+        })
     }
 }
 
--- Her-Tech-Connect/Her-Tech-Connect/TabTest.swift
+++ Her-Tech-Connect/Her-Tech-Connect/TabTest.swift
@@ -4,17 +4,26 @@
 //
 //  Created by Student Account on 10/24/21.
 //  Modified by Arica Conrad on 12/15/21.
+//  Modified by Natalman Nahm on 04/22/22
 
 import SwiftUI
+import FirebaseAuth
+import FirebaseDatabase
 
 struct TabTest: View {
+    @ObservedObject var newShoutouts: AllShoutOuts = AllShoutOuts()
+    let ref = Database.database().reference()
     var body: some View {
-        
         TabView {
             
-            HomePage().tabItem {
+            HomePage(newShoutouts: newShoutouts).tabItem {
                 Label("Home", systemImage: "house")
             }
+            .onAppear(perform: {
+                if newShoutouts.Shoutdata.isEmpty{
+                    getEarlyShoutOut()
+                }
+            })
             
             NetworkView().tabItem{
                 Label("Network", systemImage: "person.3.fill")
@@ -32,6 +41,26 @@ struct TabTest: View {
                 Label("More", systemImage: "line.horizontal.3")
             }
         }
+    }
+    
+    //load the view with existing shoutouts
+    func getEarlyShoutOut() {
+        let sorted = ref.child("ShoutOut").queryOrdered(byChild: "timeCreated")
+        sorted.observeSingleEvent(of: .value, with: {(post) in
+            for aPost in post.children {
+                let snap = aPost as! DataSnapshot
+                let postDict = snap.value as! [String: Any]
+                
+                if let image = postDict["image"] as? String {
+                    newShoutouts.Shoutdata.append(ShoutOut(shoutOutID: postDict["shoutOutID"] as! String, posterID: postDict["posterID"] as! String, title: postDict["title"] as! String, story: postDict["story"] as! String, timeCreated: postDict["timeCreated"] as! Double, upvotes: postDict["upVotes"], image: image))
+                } else {
+                    newShoutouts.Shoutdata.append(ShoutOut(shoutOutID: postDict["shoutOutID"] as! String, posterID: postDict["posterID"] as! String, title: postDict["title"] as! String, story: postDict["story"] as! String, timeCreated: postDict["timeCreated"] as! Double, upvotes: postDict["upVotes"], image: ""))
+
+                }
+            }
+            newShoutouts.Shoutdata.reverse()
+            
+        })
     }
 }
 
